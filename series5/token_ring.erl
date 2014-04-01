66434304e66bec3148b5ca4c4badd0b8873fa649
lexruee <a.rueedlinger@gmail.com>
2014-04-01 11:22:05 +0200

    add final solution

--- series5/token_ring.erl
+++ series5/token_ring.erl
@@ -12,14 +12,22 @@
 -export([start/3, master/3, actor/4]).
 
 
-%% @spec start(N::int(),M::int()) -> int().
+%% Creates a token ring with N processes. 
+%% The I-th process sends a token
+%% and M messagers are sent in total.
+%% @spec start(N::int(),M::int()) -> int()
+%%
 start(N,M,I) -> 
 	out("",[],write), % overwrite output file	
-	spawn(token_ring,master,[N,M,I]).
+	spawn(token_ring,master,[N,M,I]). % spawn master process.
+	
 
-% Deploys n processes.
+%% Deploys N slave processes.
+%% @spec start(N::int(),M::int()) -> pid()
+%%
 deploy(0,_,Pid) -> 
-	self() ! {deployed,Pid},
+	% inform the master (myself) that all processes are deployed.
+	self() ! {deployed,Pid}, % pid correspond to the "last" process in the ring.
 	Pid;
 
 deploy(N,M,SuccPid) ->
@@ -27,28 +35,31 @@ deploy(N,M,SuccPid) ->
 	out("deployed process ~p: pid: ~p\n",[N,Pid],append),		
 	deploy(N-1,M,Pid).
 
+%% Inital master function to initalize the token ring.
+%% @spec master(N::int(),M::int(),I::int()) -> any()
+%%
+master(N,M,I) ->
+	out("deployed process ~p: pid: ~p\n",[N,self()],append),
+	deploy(N-1,M,self()),
+	waitOnDeployment(N,M,I). % enter wait on deployment state.
 
-master(N,M,I) -> 
-	deploy(N,M,self()),
-	waitOnDeployment(M,I).
-
-%
-% @spec waitOnDeployment(M::int()) -> pid()
-%
-waitOnDeployment(M,I) ->
+%% Wait "loop". Waits until the token ring created.
+%% @spec waitOnDeployment(M::int()) -> any()
+%%
+waitOnDeployment(N,M,I) ->
 	receive
 		{deployed,SuccPid} -> out("all processes are deployed!\n",[],append),
 		SuccPid ! {find,I}, %find i-th node 
-		actor(I,M,SuccPid,0)
+		actor(N,M,SuccPid,0) % if found enter actor state.
 	end,
-	waitOnDeployment(M,I).
+	waitOnDeployment(N,M,I).
 
-%
-% Main actor function.
-% @spec actor(M::int(),SuccPid::pid(),Counter::int()) -> stop
-%
+%% Main actor function for slace and master process.
+%% @spec actor(M::int(),SuccPid::pid(),Counter::int()) -> any()
+%%
 actor(I,M,SuccPid,Counter) ->
 	receive	
+		% starts sending a token.		
 		% find i-th node and start counting and send token
 		{find,Id} when I == Id ->
 			out("N ~p, pid: ~p, succ pid: ~p, send token!\n",[I,self(),SuccPid],append),
@@ -57,7 +68,7 @@ actor(I,M,SuccPid,Counter) ->
 		{find,Id} when I /= Id ->
 			SuccPid ! {find,Id}; 
 
-
+		% increments a counter.
 		% if a counter message is received then increment the counter and
 		% send it to its successor.		
 		{counter,CounterValue,MasterPid,token} when CounterValue < M -> 
@@ -68,12 +79,14 @@ actor(I,M,SuccPid,Counter) ->
 		{counter,CounterValue,MasterPid,token} when CounterValue==M ->
 			MasterPid ! {stopped};
 
+		% initalizes the collection phase.
 		% if the master receives a stopped message 
 		% then start collecting the current states of each ring process.
 		{stopped} -> 
 			out("received stopped message!\n",[],append),
 		SuccPid ! {state,self()};
 
+		% collects state information.
 		% if a process receives a state message then send back 
 		% the master its current state
 		{state,MasterPid} when MasterPid /= self() ->
@@ -85,6 +98,7 @@ actor(I,M,SuccPid,Counter) ->
 			MasterPid ! {collect,self(),SuccPid,Counter},
 			shutdown();
 
+		% summarizes the collected data.
 		% if the master receives a collect message
 		% then write the results into a file
 		{collect,Pid,SPid,CValue} ->
@@ -93,15 +107,16 @@ actor(I,M,SuccPid,Counter) ->
 	end, 
 	actor(I,M,SuccPid,Counter).
 
+%% Stop function.
+%% Represents a final state.
 shutdown() -> stop.
 
 
-% Writes a formatted string into a file.
-% @spec actor(Str::string(),L::list(),Mode::atom()) -> stop
-%
+%% Writes a formatted string into a file.
+%% @spec actor(Str::string(),L::list(),Mode::atom()) -> any()
+%%
 out(Str,L,Mode) ->
 	{ok, IO} = file:open("./tr.hrl",[Mode]),
-	io:fwrite(IO,Str,L),
-	stop.
+	io:fwrite(IO,Str,L).
 
 
