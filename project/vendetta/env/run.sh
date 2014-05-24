53fbf68a6dbabe4716b70fb51d4d3f1659d4128e
lexruee <a.rueedlinger@gmail.com>
2014-05-24 15:29:42 +0200

    remove vendetta folder

--- project/vendetta/env/run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# author:   Alexander Rüedlinger, Michael Jungo
-# date:     2014
-
-HOST=$(uname -n)
-erl -name "master@$HOST" -setcookie "$1" -noshell -eval "$2,init:stop()."
--- project/vendetta/env/run_remote.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# author:   Alexander Rüedlinger, Michael Jungo
-# date:     2014
-
-HOST=$(uname -n)
-ssh "$1" "cd $2 && erl +P $3 -name $4 -setcookie $5 -detached"
